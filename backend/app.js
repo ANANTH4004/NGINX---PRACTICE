d38c7403acd103da6c90f0bebfe829b25d700cff
ANANTH4004 <balamuruganananth@gmail.com>
2025-10-30 11:13:24 +0530

    4 backend containers in different ports

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const PORT = 3000
+const port = process.env.PORT || 3000
 
-app.get('/api', (req, res) => res.json({ message: `Hello from Node! ${PORT}` }))
+app.get('/api', (req, res) => res.json({ message: `Hello from Node! ${port}` }))
 
-app.listen(PORT, () => console.log('Server running on port 3000'))
+app.listen(port, () => console.log(`Server started on port ${port}`))
