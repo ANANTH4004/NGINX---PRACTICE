b58b0ca80a0498fba52a1ae9b9df0c41e6ccb1d0
ANANTH4004 <balamuruganananth@gmail.com>
2025-10-30 23:43:04 +0530

    nginx as load balancer

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => res.json({ message: `Hello from Node! ${port}` }))
+
 app.get('/api', (req, res) => res.json({ message: `Hello from Node! ${port}` }))
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
